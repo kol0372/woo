58120d40cdad918e4c949278116f3ab1805db69f
kol0372 <kol7456@naver.com>
2014-09-16 13:33:09 +0900

    java0901_start

---
+++ java0901_start/src/ex1/NewClass.java
@@ -0,0 +1,12 @@
+
+package ex1;
+
+/**
+ * @author kosta
+ * Document    : NewClass created on : 2014. 9. 11, ¿ÀÀü 9:41:55
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("TT");
+    }
+}
---
+++ java0901_start/src/exam/ExamProfile.java
@@ -0,0 +1,14 @@
+
+package exam;
+
+
+public class ExamProfile {
+
+public static void main(String[] args) {
+    System.out.println("이름 : 김우리");
+    System.out.println("나이 : 25");
+    System.out.println("주소 : 성남시 수정구 태평4동 7317-13");
+    System.out.print("성별 : ");
+    System.out.println("여");
+}
+}
