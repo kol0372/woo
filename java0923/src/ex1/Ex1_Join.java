15211c99c78988f255e010b105a86b8ad47ec0af
kol0372 <kol7456@naver.com>
2014-09-23 19:11:28 +0900

    java0922

--- java0923/src/ex1/Ex1_Join.java
+++ java0923/src/ex1/Ex1_Join.java
@@ -12,15 +12,16 @@ public class Ex1_Join {
         Ex1_Thread1 t1 = new Ex1_Thread1();
         startTime = System.currentTimeMillis();
         t1.start();
-        t1.join(); // t1의 작업이 끝날때 까지 다른 스레드는 대기.
+        t1.join(); 
+        // t1의 작업이 끝날때 까지 다른 스레드는 대기.
         //runnable 인터페이스를 구현한 run만 가지고 있어서 start를 호출할 수 없다.
-        //run 호출은 일을시키는게 아니고, 메인이 직접이라서 스레드가 아니라고 볼수 있다.
-        //스레드를 일시키려면 start메서드에서 
         
+        //run 호출은 일을시키는게 아니고, 메인이 직접이라서 스레드가 아니라고 볼수 있다.
+               
         //**스레드를 생성해서 생성자인자에 러너블을 구현한 클래스의 주소를 넣어줘야햄.**
-        Ex1_Thread2 t2 = new Ex1_Thread2();
-        Thread tt2 = new Thread(t2);
-        tt2.start();
+        Ex1_Thread2 t2 = new Ex1_Thread2(); // t2가 스레드가 아니라 러너블
+        Thread tt2 = new Thread(t2); //러너블(t2)을 스레드로 만듬.
+        tt2.start(); //스레드로 했기때문에 start 가능.
         tt2.join(); //스레드간의 순서를 정해 줄 수 있따.
         System.out.println("걸린시간 : " + (System.currentTimeMillis()-startTime));
     }
